065b8bc3a8f37fe3aba223fa81636394a5089d4e
ran <lh.song@impix.co.kr>
2021-06-07 13:34:04 +0900

    &&이용 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React, { Fragment } from 'react';
 
 
 function App() {
-  const name ='react';
-  return (
-    <div>
-      { //조건부 연산자 
-        name === 'react' ? (
-          <h1>리액트 입니다.</h1>
-        ) : (
-          <h1>리액트가 아닙니다.</h1>
-        )
-      }
-    </div>  
-    );
+  // const name ='react';
+  // return (
+    // <div>
+    //   { //조건부 연산자 
+    //     name === 'react' ? (
+    //       <h1>리액트 입니다.</h1>
+    //     ) : (
+    //       <h1>리액트가 아닙니다.</h1>
+    //     )
+    //   }
+    // </div> 
+    // );
+    // return <div>{name==='react'?<h1>리액트입니다.</h1>:null}</div>
+    // 여러줄은 {}로 감싸고 한줄은 감싸지 않는다.
+    // return <div>{name==='react' && <h1>리액트입니다.</h1>}</div>
 }
 
 export default App;
