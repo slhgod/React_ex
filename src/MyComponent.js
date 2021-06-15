d6dcca9f108d64c5bdf09bdd96c9d21f1e3d93be
ran <lh.song@impix.co.kr>
2021-06-15 13:48:54 +0900

    state

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,26 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import propsTypes from "prop-types";
+import { render } from "@testing-library/react";
 
-const MyComponent = (props) => {
-  const { name, children, favoriteNumber } = props;
-  return (
-    <div>
-      안녕, 내 이름은 {name}이야.
-      <br />
-      children 값은 {children}이야.
-      <br />
-      내가 좋아하는 숫자는 {favoriteNumber}야
-    </div>
-  );
-};
+class MyComponent extends Component {
+  static defaultProps = {
+    name: "기본 이름",
+  };
 
-MyComponent.defaultProps = {
-  name: "기본 이름",
-};
+  static propsTypes = {
+    name: propsTypes.string,
+    favoriteNumber: propsTypes.number.isRequired,
+  };
 
-MyComponent.propsTypes = {
-  name: propsTypes.string,
-  favoriteNumber: propsTypes.number.isRequired,
-};
+  render() {
+    const { name, children, favoriteNumber } = this.props;
+    return (
+      <div>
+        안녕, 내 이름은 {name}이야.
+        <br />
+        children 값은 {children}이야.
+        <br />
+        내가 좋아하는 숫자는 {favoriteNumber}이야
+      </div>
+    );
+  }
+}
 
 export default MyComponent;
---
+++ src/counter.js
@@ -0,0 +1,30 @@
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+
+// 105p.
+class Counter extends Component {
+  Constructor(props) {
+    super(props);
+    //state의 초기값 설정하기
+    this.state = {
+      number: 0,
+    };
+  }
+  render() {
+    const { number } = this.state; // state를 조회할 때는 this.state로 조회
+    return (
+      <div>
+        <h1>{number}</h1>
+        <button
+          onClick={() => {
+            this.setState({ number: number + 1 });
+          }}
+        >
+          +1
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
