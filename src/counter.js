1b09ac7b02f267ba752374ad809e838ee12f56f8
ran <lh.song@impix.co.kr>
2021-06-21 11:36:52 +0900

    this.setState 끝난 후 특정 작업 실행하기

--- src/counter.js
+++ src/counter.js
@@ -8,16 +8,21 @@ class Counter extends Component {
     //state의 초기값 설정하기
     this.state = {
       number: 0,
+      fixedNumber: 0,
     };
   }
   render() {
-    const { number } = this.state; // state를 조회할 때는 this.state로 조회
+    const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           onClick={() => {
-            this.setState({ number: number + 1 });
+            this.setState({ number: number + 1 }, () => {
+              console.log("방금 setState가 호출되었습니다.");
+              console.log(this.state);
+            });
           }}
         >
           +1
